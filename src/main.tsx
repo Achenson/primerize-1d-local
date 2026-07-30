69ea3c6413c420fa9cbd9694ea4b18dcfe9c4bfd
Achenson <ach.01.mail@gmail.com>
2026-07-30 14:35:35 +0200

    global.d.ts src

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App'; // TypeScript infers .tsx files automatically; omit the extension
+import App from './App';
 
 const rootElement = document.getElementById('root');
 
