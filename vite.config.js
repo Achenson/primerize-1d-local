fff01fd1d3375f05ee6bd9112e0ff9206e9afdc8
Achenson <ach.01.mail@gmail.com>
2026-07-31 16:15:09 +0200

    vite config production ready

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,11 @@
-import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react'; import tailwindcss from '@tailwindcss/vite';
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig(({ mode }) => {
+  return {
+    // 'mode' is explicitly passed by Vite ('development' or 'production')
+    base: mode === 'production' ? '/primerize-1d-local/' : '/',
+    plugins: [react(), tailwindcss()],
+  };
+});
